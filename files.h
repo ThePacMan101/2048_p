8ffc4d0a0569764890704ecb0830868419449f37
Paulo Roberto Godoy <130581249+paulorfgm@users.noreply.github.com>
2023-12-07 18:42:38 -0300

    commit commented 2

--- files.h
+++ files.h
@@ -7,10 +7,7 @@
 The initFiles function opens every file that will be further used in the program.
 If they don't exist yet, the function creates them.
 */
-/*
-The initFiles function opens every file that will be further used in the program.
-If they don't exist yet, the function creates them.
-*/
+
 void initFiles();
 
 /*
@@ -19,12 +16,7 @@ and translates them into a vector, then returns the pointer to the said vector.
 Besides that, it also deposits the size of the vector into a variable wich pointer
 is passed as an argument.
 */
-/*
-The userListToArray function takes all the players from the players.dat file
-and translates them into a vector, then returns the pointer to the said vector.
-Besides that, it also deposits the size of the vector into a variable wich pointer
-is passed as an argument.
-*/
+
 tUser *userListToArray(int *size);
 
 /*
@@ -32,20 +24,13 @@ The saveUserList function reveives a user and adds their name to the
 players.dat file.
 */
 
-/*
-The saveUserList function reveives a user and adds their name to the
-players.dat file.
-*/
 void saveUserList(tUser user);
 
 /*
 The saveUser function, similarly saves a user's information in a file. Except,
 this time, in a unique file for that specific player.
 */
-/*
-The saveUser function, similarly saves a user's information in a file. Except,
-this time, in a unique file for that specific player.
-*/
+
 void saveUser(tUser user);
 
 /*
@@ -53,21 +38,14 @@ The loadUser function receives a pointer to a tUser variable, than
 reads its name and tries to load all user data into the variable, if the file for that user
 exists. If not, then, the function creates a user by calling saveUserList and saveUser.
 */
-/*
-The loadUser function receives a pointer to a tUser variable, than
-reads its name and tries to load all user data into the variable, if the file for that user
-exists. If not, then, the function creates a user by calling saveUserList and saveUser.
-*/
+
 void loadUser(tUser *user);
 
 /*
 The function sortUsersByName receives an array of tUser variables and its size.
 Then, it uses the bubble sort method to sort the array based on the function strcmp.
 */
-/*
-The function sortUsersByName receives an array of tUser variables and its size.
-Then, it uses the bubble sort method to sort the array based on the function strcmp.
-*/
+
 void sortUsersByName(tUser *array, int size);
 
 /*
@@ -75,11 +53,7 @@ The sortUsersByHighScore function receives a pointer to the size variable, then
 creates an array of tUser variables pulled from the players file and uses the bubble sort
 method to sort the array using the players' high score as criteria.
 */
-/*
-The sortUsersByHighScore function receives a pointer to the size variable, then
-creates an array of tUser variables pulled from the players file and uses the bubble sort
-method to sort the array using the players' high score as criteria.
-*/
+
 tUser *sortUsersByHighScore(int *size);
 
 /*
@@ -95,11 +69,7 @@ The deleteUser function takes a player's name, then deletes the register
 of this player out of the players file and deletes the unique file for the player.
 It uses the binSearchName to find the player in the file based on their name.
 */
-/*
-The deleteUser function takes a player's name, then deletes the register
-of this player out of the players file and deletes the unique file for the player.
-It uses the binSearchName to find the player in the file based on their name.
-*/
+
 bool deleteUser(char *name);
 
 #endif
